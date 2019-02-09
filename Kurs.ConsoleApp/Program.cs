152f25b851a06badb111b2c785e37e510362300a
adminek <adminek@ti.niepolomice>
2019-02-09 11:24:06 +0100

    Add project files.

---
+++ Kurs.ConsoleApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kurs.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Podaj a: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("Podaj b: ");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine(a+" + "+b+" = "+(a+b));
+            Console.Write("Podaj swoje imie: ");
+            string imie = Console.ReadLine();
+            Console.Write("Podaj swoje nazwisko: ");
+            string nazwisko = Console.ReadLine();
+            Console.WriteLine("Witaj {0} {1} "+DateTime.Now.Date.ToLongDateString(),imie,nazwisko);
+            Console.ReadKey();
+        }
+    }
+}
