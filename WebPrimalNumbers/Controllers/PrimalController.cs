fc59ef47e66d8e18dc199ce3ba32fcea1a5f0ee1
Paweł Szczeszek <pszczesz@poczta.onet.pl>
2019-03-02 13:20:00 +0100

    d2

--- WebPrimalNumbers/Controllers/PrimalController.cs
+++ WebPrimalNumbers/Controllers/PrimalController.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
+using Kurs.Primary;
+using WebPrimalNumbers.Models;
 
 namespace WebPrimalNumbers.Controllers
 {
@@ -11,16 +14,31 @@ namespace WebPrimalNumbers.Controllers
         // GET: Primal
         public ActionResult Index()
         {
-           
+            Primal primal = new Primal();
             return View();
         }
 
         // POST: Primal
         [HttpPost]
-        public ActionResult Index(int liczba)
+        public ActionResult Index(Primal primal)
         {
-
-            return View();
+            if (ModelState.IsValid)
+            {
+                int liczba = primal.Liczba;
+                PrimaryNumbers pn = new PrimaryNumbers(liczba);
+                StringBuilder sb = new StringBuilder();
+                foreach (var item in pn.GetNumbers())
+                {
+                    sb.Append(item + " ");
+                }
+                ViewBag.liczby = sb.ToString();
+                return View();
+            }
+            else
+            {
+                return View(primal);
+            }
+           
         }
     }
 }
---
+++ WebPrimalNumbers/Models/Primal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using Kurs.Primary;
+
+namespace WebPrimalNumbers.Models
+{
+    public class Primal
+    {
+       
+        [Required(ErrorMessage = "Podaj liczbę")]
+        [Range(minimum:2,maximum:20000)]
+        public int Liczba { get; set; }
+    
+    }
+}
