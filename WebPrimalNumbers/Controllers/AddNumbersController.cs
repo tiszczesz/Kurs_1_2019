3552d655521cf5b96ce8bb094875945f805eeb64
Paweł Szczeszek <pszczesz@poczta.onet.pl>
2019-03-23 09:31:33 +0100

    dodawanie1

---
+++ WebPrimalNumbers/Controllers/AddNumbersController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebPrimalNumbers.Models;
+
+namespace WebPrimalNumbers.Controllers
+{
+    public class AddNumbersController : Controller
+    {
+        // GET: AddNumbers
+        public ActionResult Index()
+        {
+            var addNumbers = new AddNumbers();
+            return View(addNumbers);
+        }
+
+        //POST : AddNumbers
+        [HttpPost]
+        public ActionResult Index(AddNumbers addNumbers)
+        {
+            int wynik = addNumbers.GetSum();
+            ViewBag.wynik = wynik;
+            return View(addNumbers);
+        }
+    }
+}
---
+++ WebPrimalNumbers/Models/AddNumbers.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebPrimalNumbers.Models
+{
+    public class AddNumbers
+    {
+        [Display(Name="Podaj A")]
+        [Required(ErrorMessage = "Podaj liczbę całkowitą")]
+        public int A { get; set; }
+
+        [Display(Name = "Podaj B")]
+        [Required(ErrorMessage = "Podaj liczbę całkowitą")]
+        public int B { get; set; }
+
+        public int GetSum()
+        {
+            return A + B;
+        }
+    }
+}
